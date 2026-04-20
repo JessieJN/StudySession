683ebf21e4810508fc432c98acf314a0496d1c2d
Jessie <jessiejarl@hotmail.com>
2026-04-20 09:34:41 +0200

    Initial backend setup with MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,19 @@
+
+require("dotenv").config();
+
+const mongoose = require("mongoose")
+const app = require("./app");
+
+const PORT = 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.log("MongoDB connection error;", err)
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
